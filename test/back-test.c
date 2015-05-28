a98cb77aaad4fc67a7eddbe65a577045ee9e3fef
Andrew Keesler <andrew.keesler@silabs.com>
2015-05-28 19:32:15 -0400

    Fix the back-test.
    - Still using loopback to communicate since I'm a school girl bitch
      and can't get UNIX sockets to work...

--- test/back-test.c
+++ test/back-test.c
@@ -13,6 +13,7 @@
 #include <stdint.h>
 #include <assert.h>
 #include <string.h> // memcmp
+#include <unistd.h> // close
 
 #include <sys/socket.h> // socket, recvfrom
 #include <netinet/in.h> // INADDR_LOOPBACK, sockaddr_in
@@ -27,14 +28,51 @@
 #define note(x)   \
   do { printf("(%s = %d)", #x, x); } while (0)
                                          
-#define PORT 12345
+#define PORT 55555
 
 #define SMALL_BYTES_COUNT (5)
 static uint8_t smallBytes[SMALL_BYTES_COUNT] = { 1, 2, 3, 4, 5, };
 
+// -----------------------------------------------------------------------------
+// Fake parent stuff
+
+static int fakeParentSock;
+
+static void startFakeParent(void)
+{
+  struct sockaddr_in loopbackAddress;
+
+  if ((fakeParentSock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+    note(errno);
+    assert(0);
+  }
+
+  loopbackAddress.sin_family = AF_INET;
+  loopbackAddress.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  loopbackAddress.sin_port = htons(PORT);
+  if (bind(fakeParentSock,
+           (struct sockaddr *)&loopbackAddress,
+           sizeof(loopbackAddress))
+      == -1) {
+    note(errno);
+    assert(0);
+  }
+}
+
+static void receiveBytes(uint8_t *bytes, uint8_t count)
+{
+  if (recv(fakeParentSock, bytes, count, 0) == -1) {
+    note(errno);
+    assert(0);
+  }
+}
+
+// -----------------------------------------------------------------------------
+// Tests
+
 static void socketTest(void)
 {
-  //uint8_t rxBuffer[256];
+  uint8_t rxBuffer[256];
   int err;
 
   // Make sure the socket initializes correctly.
@@ -46,10 +84,8 @@ static void socketTest(void)
   expect(!err);
 
   // Receive those bytes.
-  // FIXME:
-  //err = ipcReceive(rxBuffer, SMALL_BYTES_COUNT);
-  //note(err);
-  //expect(memcmp(rxBuffer, smallBytes, SMALL_BYTES_COUNT) == 0);
+  receiveBytes(rxBuffer, SMALL_BYTES_COUNT);
+  expect(memcmp(rxBuffer, smallBytes, SMALL_BYTES_COUNT) == 0);
 
   // Close the socket.
   ipcDeinit();
@@ -57,6 +93,11 @@ static void socketTest(void)
 
 int main(void)
 {
+  startFakeParent();
+
   run(socketTest);
+
+  close(fakeParentSock);
+
   return 0;
 }
